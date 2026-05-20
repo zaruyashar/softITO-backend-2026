1d2989c822062bbc98da004a3eed9af65618eaca
zaruyashar <203256006+zaruyashar@users.noreply.github.com>
2026-05-20 10:16:25 +0300

    update-delete-search functions added

--- DbAppADONET/Form1.Designer.cs
+++ DbAppADONET/Form1.Designer.cs
@@ -174,6 +174,7 @@
             this.button3.TabIndex = 13;
             this.button3.Text = "Güncelle";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -184,6 +185,7 @@
             this.button4.TabIndex = 14;
             this.button4.Text = "Sil";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -194,6 +196,7 @@
             this.button5.TabIndex = 15;
             this.button5.Text = "Arama Yap";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
--- DbAppADONET/Form1.cs
+++ DbAppADONET/Form1.cs
@@ -30,17 +30,13 @@ namespace DbAppADONET
             dataGridView1.DataSource = ds.Tables[0];
         }
 
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
+        // List
         private void button1_Click(object sender, EventArgs e)
         {
             Listele("SELECT * FROM Malzemeler");
         }
 
+        // Save
         private void button2_Click(object sender, EventArgs e)
         {
             conn.Open();
@@ -59,14 +55,67 @@ namespace DbAppADONET
             conn.Close();
         }
 
+        // Update
+        private void button3_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            SqlCommand command = new SqlCommand("UPDATE Malzemeler SET MalzemeAdi = '"+textBox2.Text.ToString()+ "', Fiyat = '" + textBox3.Text.ToString() + "', Adet = '"+ textBox4.Text.ToString()+ "', Durum = '"+ textBox5.Text.ToString()+ "' WHERE MalzemeNo = '"+ textBox1.Text.ToString()+ "'", conn);
+
+            command.ExecuteNonQuery();
+            Listele("SELECT * FROM Malzemeler");
+
+            conn.Close();
+        }
+
+        // Delete
+        private void button4_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            SqlCommand command = new SqlCommand("DELETE FROM Malzemeler WHERE MalzemeNo = @MalzemeNo", conn);
+            command.Parameters.AddWithValue("@MalzemeNo", textBox1.Text);
+
+            command.ExecuteNonQuery();
+            Listele("SELECT * FROM Malzemeler");
+
+            conn.Close();
+        }
+
+        // Search
+        private void button5_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Malzemeler WHERE MalzemeAdi LIKE '%"+textBox2.Text+"%'", conn);
+
+            SqlDataAdapter dr = new SqlDataAdapter(command);
+            DataSet ds = new DataSet();
+            dr.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+
+            conn.Close();
+        }
+
+
+
+
+        // DatagridView
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             int satir = dataGridView1.SelectedCells[0].RowIndex;
-            textBox1.Text = dataGridView1.Rows[satir].Cells[0].Value.ToString();
             textBox2.Text = dataGridView1.Rows[satir].Cells[1].Value.ToString();
             textBox3.Text = dataGridView1.Rows[satir].Cells[2].Value.ToString();
             textBox4.Text = dataGridView1.Rows[satir].Cells[3].Value.ToString();
             textBox5.Text = dataGridView1.Rows[satir].Cells[4].Value.ToString();
+        }
+
+
+
+        // Yanlış tıklamalar
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
